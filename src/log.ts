ebe1e859c3abc101bcdc9ec78d40ea2449a5add3
Romain Champourlier <public@rchampourlier.com>
2020-12-08 10:48:58 +0100

    Improved log function
    - Added version number
    - Fixed handling variable arguments

--- src/log.ts
+++ src/log.ts
@@ -1,4 +1,6 @@
-export function log(msg: string, args?: any) {
-  if (args !== undefined) console.log(`${(new Date()).toISOString()} -- ${msg}`, ...args)
-  else console.log(`${(new Date()).toISOString()} -- ${msg}`)
+const Version = '1.0.0'
+
+export function log(msg: string, ...args: any[]) {
+  if (args.length > 0) console.log(`v${Version} -- ${(new Date()).toISOString()} -- ${msg}`, ...args)
+  else console.log(`v${Version} -- ${(new Date()).toISOString()} -- ${msg}`)
 }
