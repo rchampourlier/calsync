c51dc767259800e5664586a49b987365885272b1
Romain Champourlier <public@rchampourlier.com>
2021-02-05 23:12:27 +0100

    Changes for local project path change

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 NODE_DIR=$HOME/.nvm/versions/node/v14.10.1/bin
-APP_DIR=$HOME/Dev/+projects/calsync
+APP_DIR=$HOME/Dev/_projects/calsync
 NODE=$NODE_DIR/node
 APP=$APP_DIR/dist/app.js
 
