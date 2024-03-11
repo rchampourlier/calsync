c6cd5552a1e58f89974f956a3a11871981ddf54d
Romain Champourlier <public@rchampourlier.com>
2024-03-11 23:05:11 +0100

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 NVM_DIR=$HOME/.nvm
-NODE_DIR=$NVM_DIR/versions/node/v14.17.4/bin
-#NODE=$NODE_DIR/node
+NODE_DIR=$NVM_DIR/versions/node/v20.8.0/bin
 NODE=node
-APP_DIR=$HOME/Dev/_projects/calsync
+APP_DIR=$HOME/Dev/_projects/calsync/calsync-bg
 APP=$APP_DIR/dist/app.js
 
 source $NVM_DIR/nvm.sh && cd $APP_DIR && $NODE $APP
